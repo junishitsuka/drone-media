53f5f8e3c1a71c4e305402f901c440f15d36ca26
junishitsuka <ishitsuka.jun@gmail.com>
2016-02-24 23:38:54 +0900

    add target table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224142806) do
+ActiveRecord::Schema.define(version: 20160224143509) do
 
   create_table "application_purposes", force: :cascade do |t|
     t.integer  "application_id", limit: 4
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20160224142806) do
   end
 
   create_table "purposes", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "targets", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Purpose.connection.execute("TRUNCATE TABLE purposes;")
+Target.connection.execute("TRUNCATE TABLE targets;")
+
 Purpose.create([
   {name: '企画プロモーション'},
   {name: '記念撮影'},
   {name: '官庁撮影'},
   {name: 'テレビ撮影'},
   {name: '測量'}
+])
+
+Target.create([
+  {name: '建造物'},
+  {name: '広大な自然'},
+  {name: '鉄橋'},
+  {name: '製品'},
+  {name: '人'},
+  {name: '街並み'},
+  {name: 'その他'}
 ])
